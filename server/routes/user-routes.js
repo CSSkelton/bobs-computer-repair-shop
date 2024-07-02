5e60b072162c093aaea63121ea2cd73c4259d448
CSSkelton <codysskelton@gmail.com>
2024-07-01 22:40:13 -0400

    mongodb connection string

---
+++ server/routes/user-routes.js
@@ -0,0 +1,10 @@
+/**
+ * Title: user-routes.js
+ * Author: Cody Skelton
+ * Date: 07.01.2024
+ */
+"use strict";
+
+// Add mongo.js document variables
+const { mongo } = require("../utils/mongo");
+const { ObjectId } = require('mongodb');
