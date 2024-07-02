093b219c81745ab1b4d008f8158b76325e1ceab6
Jeremy Lates <jlates@my365.bellevue.edu>
2024-07-02 07:58:37 -0400

    adding findById to user-routes.js

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -8,7 +8,7 @@
 const express = require("express");
 // Add mongo.js document variables
 const { mongo } = require("../utils/mongo");
-const { ObjectId } = require('mongodb');
+const { ObjectId } = require("mongodb");
 
 const createError = require("http-errors");
 const router = express.Router();
@@ -34,20 +34,66 @@ const router = express.Router();
  */
 router.get("/", (req, res, next) => {
   try {
+    mongo(async (db) => {
+      const users = await db.collection("users").find().toArray();
 
-    mongo(async db => {
-      const users = await db.collection('users').find().toArray();
-
-      console.log('users', users);
+      console.log("users", users);
 
       if (!users) {
-        return next(createError(404, 'No employees found'));
+        return next(createError(404, "No employees found"));
       }
 
       res.send(users);
     }, next);
   } catch (err) {
-    console.error('err', err);
+    console.error("err", err);
+    next(err);
+  }
+});
+
+/**
+ * findUserById
+ * @openapi
+ * /api/users/{email}:
+ *   get:
+ *     tags:
+ *       - Users
+ *     description:  API for returning a user document
+ *     summary: returns a user document
+ *     parameters:
+ *       - name: email
+ *         in: path
+ *         required: true
+ *         description: Users document email
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: User document
+ *       '404':
+ *         description: User not found
+ *       '500':
+ *         description: Server exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.get("/:email", (req, res, next) => {
+  try {
+    let { email } = req.params;
+
+    //Database query is handled
+    mongo(async (db) => {
+      const user = await db.collection("users").findOne({ email });
+
+      if (!user) {
+        console.error("User email not found:", email);
+        return next(createError(404, "User not found"));
+      }
+
+      res.send(user);
+    }, next);
+  } catch (err) {
+    console.error("Error: ", err);
     next(err);
   }
 });
