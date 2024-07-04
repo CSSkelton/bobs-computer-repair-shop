f3cd9846937d220134ccfd27be10fb2181e73e03
CSSkelton <codysskelton@gmail.com>
2024-07-03 22:14:29 -0400

    role guard - minor todos

--- src/app/admin-routing.module.ts
+++ src/app/admin-routing.module.ts
@@ -1,12 +1,30 @@
-import { NgModule } from '@angular/core';
+/**
+ * Title: admin-routing.module.ts
+ * Author: Cody Skelton
+ * Date: 07.03.2024
+ */
+
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin/admin.component';
+import { roleGuard } from './shared/role.guard';
 
 
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [],
+    canActivate: [roleGuard]
+  }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forChild(routes),
+  ],
+  exports: [RouterModule]
 })
 export class AdminRoutingModule { }
