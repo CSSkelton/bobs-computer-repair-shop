230bd13139a251240d941882588d96adac0464d1
George <george_rtaylor@yahoo.com>
2024-07-07 16:20:51 -0500

    updating coding for employee form

--- src/app/admin/create-employee/create-employee.component.ts
+++ src/app/admin/create-employee/create-employee.component.ts
@@ -16,5 +16,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent {
+  errorMessage: string // holds error messages
+
+   //employee form with validation rules for each field
+   employeeForm: FormGroup = this.fb.group({
+    empId: [null, Validators.compose([Validators.required, Validators.pattern('^[0-9]*$')])],
+    firstName: [null, Validators.compose([Validators.required])],
+    lastName: [null, Validators.compose([Validators.required])],
+    email: [null, Validators.compose([Validators.required, Validators.email])],
+    // password field requires one uppercase character and one number (min 8 characters)
+    password: [null, Validators.compose([Validators.required, Validators.pattern('^(?=.*[0-9](?=.*[a-z](?=.*[A-Z](?=.*[a-zA-Z].{8,}$')])],
+    role: [null, Validators.compose([Validators.required])]
+})
+
+// injects FormBuilder, Router, and EmployeeService
+constructor(private fb: FormBuilder, private router: Router,){
+  this.errorMessage ='' //set error Message to empty string
+
 
 }
