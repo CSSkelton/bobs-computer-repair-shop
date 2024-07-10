b97b2406a118249a6a60d40ffa913f118d29cbf3
George <george_rtaylor@yahoo.com>
2024-07-10 00:34:15 -0500

    updated

--- src/app/admin/employee/create-employee/create-employee.component.ts
+++ src/app/admin/employee/create-employee/create-employee.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
-export class CreateEmployeeComponent {
+export class CreateEmployeeComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+  saveEmployee(empForm: NgForm): void {
+    console.log(empForm);
+  }
 
 }
