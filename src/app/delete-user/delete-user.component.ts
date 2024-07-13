126bdb2254be0aa593a032150132930012b351be
George <george_rtaylor@yahoo.com>
2024-07-13 11:42:31 -0500

    updated api information for delete employee

--- src/app/delete-user/delete-user.component.ts
+++ src/app/delete-user/delete-user.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+/**
+ * Title:delete-user.component.ts
+ * Author: Professor Krasso
+ * Editor: George Taylor
+ * Date: 07.13.2024
+ * Description: Removes users from the system
+ */
+
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete-user',
   templateUrl: './delete-user.component.html',
   styleUrls: ['./delete-user.component.css']
 })
-export class DeleteUserComponent {
+export class DeleteUserComponent implements OnInit{
+  employee: string;
+
+  constructor(private dialogRef: MatDialogRef<DeleteUserComponent>, @Inject(MAT_DIALOG_DATA)data){
+    this.employee = data.employee;
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
