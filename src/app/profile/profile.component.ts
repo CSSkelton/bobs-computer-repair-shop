63e02df0a146cb32e150cc35450b434fc25c207d
George <george_rtaylor@yahoo.com>
2024-07-19 01:15:36 -0500

    updating profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,17 @@
 /**
  * Title: profile.component.ts
  * Author: George Taylor
- * Date: 07.16.2024
+ * Date: 07.19.2024
  */
 
 
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { UserInterface } from '../user-interface';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-profile',
