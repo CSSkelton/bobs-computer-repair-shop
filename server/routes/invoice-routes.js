e69998fa0e9bcc3b1915bcac67405865ba0a21d4
CSSkelton <codysskelton@gmail.com>
2024-07-20 05:34:12 -0400

    findPurchasesByServiceAPI

---
+++ server/routes/invoice-routes.js
@@ -0,0 +1,78 @@
+/**
+ * Title: invoice-routes.js
+ * Author: Cody Skelton
+ * Date: 07.20.2024
+ */
+"use strict";
+
+const express = require("express");
+// Add mongo.js document variables
+const { mongo } = require("../utils/mongo");
+const { ObjectId } = require("mongodb");
+
+const createError = require("http-errors");
+const router = express.Router();
+
+/**
+ * findPurchasesByService
+ * @openapi
+ * /api/invoices/purchases-graph:
+ *  get:
+ *   description: Returns count of each service bought
+ *   summary: Return purchase counts
+ *   responses:
+ *    '200':
+ *     description: Requested data
+ *    '400':
+ *     description: Bad Request
+ *    '500':
+ *     description: Internal Server Error
+ *   tags:
+ *    - Invoice
+ */
+
+//TODO: Refactor database to show line items with price and title properties
+router.get('/purchases-graph', (req, res, next) => {
+  try {
+    mongo(async (db) => {
+      const data = await db.collection("users").aggregate([
+        {
+          $unwind: '$lineItems'
+        },
+        {
+          $group:
+          {
+            '_id':
+            {
+              'title': '$lineItems.title',
+              'price': '$lineItems.price'
+            },
+            'count':
+            {
+              $sum: 1
+            }
+          }
+        },
+        {
+          $sort:
+          {
+            '_id.title': 1
+          }
+        }
+      ])
+
+      if (!data) {
+        return next(createError(400, 'Data not found'))
+      }
+
+      console.log(data);
+
+      res.status(200).send(data);
+    }, next);
+
+  } catch (err) {
+    return next(createError(500, 'Server Error'))
+  }
+})
+
+module.exports = router;
