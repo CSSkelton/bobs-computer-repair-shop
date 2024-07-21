1abe7cff05a4d87785169d5ebf0e02a9b99e938a
CSSkelton <codysskelton@gmail.com>
2024-07-21 12:56:56 -0400

    graph container styling

--- src/app/admin/service-graph/service-graph.component.ts
+++ src/app/admin/service-graph/service-graph.component.ts
@@ -4,13 +4,39 @@
  * Date: 07.20.2024
  */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-service-graph',
   templateUrl: './service-graph.component.html',
   styleUrls: ['./service-graph.component.css']
 })
-export class ServiceGraphComponent {
+export class ServiceGraphComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+      const chart = new Chart("serviceGraph", {
+        type: 'pie',
+        data: {
+          labels: ['Password Reset', 'Spyware Removal', 'RAM Upgrade', 'Software Installation', 'PC Tune-up', 'Keyboard Cleaning', 'Disk Clean-up'],
+          datasets: [{
+            //TODO: Replace with actual data once API is fixed
+            data: [5, 13, 5, 17, 6, 1, 20],
+            backgroundColor: [
+              '#ED0A3F',
+              '#FF8833',
+              '#5FA777',
+              '#0066CC',
+              '#683FA0',
+              '#AF593E',
+              '#6CDAE7'
+            ]
+          }]
+        }
+  })
+  }
 
 }
